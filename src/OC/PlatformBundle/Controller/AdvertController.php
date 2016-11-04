774655a0809c37d39175c8f4496a291cdb7516cf
peter <mouezapeter@gmail.com>
2016-11-04 08:07:01 +0100

    ...form ne faisait rien encore.....traitement into Doctrine OK

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -8,22 +8,14 @@ use OC\PlatformBundle\Entity\Advert;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 //use Symfony\Component\HttpFoundation\Request;
 //use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-
-
-
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-
 use Symfony\Component\Form\Extension\Core\Type\FormType;
-
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-
 use Symfony\Component\HttpFoundation\Request;
+
 class AdvertController extends Controller {
 
     public function indexAction($page) {
@@ -96,31 +88,57 @@ class AdvertController extends Controller {
         $advert = new Advert();
 
 
-        // On crée le FormBuilder grâce au service form factory
+        // J'ai raccourci cette partie, car c'est plus rapide à écrire !
 
-        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $advert);
-
-
-        // On ajoute les champs de l'entité que l'on veut à notre formulaire
-
-        $formBuilder
+        $form = $this->get('form.factory')->createBuilder(FormType::class, $advert)
                 ->add('date', DateType::class)
                 ->add('title', TextType::class)
                 ->add('content', TextareaType::class)
                 ->add('author', TextType::class)
-                ->add('published', CheckboxType::class)
+                ->add('published', CheckboxType::class, array('required' => false))
                 ->add('save', SubmitType::class)
+                ->getForm()
 
         ;
 
-        // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
-        // À partir du formBuilder, on génère le formulaire
 
-        $form = $formBuilder->getForm();
+        // Si la requête est en POST
+
+        if ($request->isMethod('POST')) {
+
+            // On fait le lien Requête <-> Formulaire
+            // À partir de maintenant, la variable $advert contient les valeurs entrées dans le formulaire par le visiteur
+
+            $form->handleRequest($request);
 
 
-        // On passe la méthode createView() du formulaire à la vue
-        // afin qu'elle puisse afficher le formulaire toute seule
+            // On vérifie que les valeurs entrées sont correctes
+            // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+
+            if ($form->isValid()) {
+
+                // On enregistre notre objet $advert dans la base de données, par exemple
+
+                $em = $this->getDoctrine()->getManager();
+
+                $em->persist($advert);
+
+                $em->flush();
+
+
+                $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+
+                // On redirige vers la page de visualisation de l'annonce nouvellement créée
+
+                return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
+            }
+        }
+
+
+        // À ce stade, le formulaire n'est pas valide car :
+        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
+        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
 
         return $this->render('OCPlatformBundle:Advert:add.html.twig', array(
                     'form' => $form->createView(),
