7734dba5b4bfaa58997a4586b962ec8f2dfed004
peter <mouezapeter@gmail.com>
2016-11-14 12:36:21 +0100

    unit test example OK

---
+++ tests/AppBundle/Util/CalculatorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Tests\AppBundle\Util;
+
+use AppBundle\Util\Calculator;
+
+class CalculatorTest extends \PHPUnit_Framework_TestCase {
+
+    public function testAdd() {
+        $calc = new Calculator();
+        $result = $calc->add(30, 12);
+
+        // assert that your calculator added the numbers correctly!
+        $this->assertEquals(42, $result);
+    }
+
+}
